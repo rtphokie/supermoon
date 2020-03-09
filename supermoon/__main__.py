c163a8c05f661c9b555d3ab92179aed578f2e359
Tony Rice <rtphokie@gmail.com>
2020-03-09 17:19:15 -0400

    correcting calculations during times between full moon and perigee

--- supermoon/__main__.py
+++ supermoon/__main__.py
@@ -28,6 +28,7 @@ from tzlocal import get_localzone
 import sys
 import simple_cache
 import argparse
+from pprint import pprint # for debug, clearly I forgot to remove this
 
 @simple_cache.cache_it(filename=".supermoon.cache", ttl=12000)
 def next_supermoon(dt=None):
@@ -45,8 +46,9 @@ def next_supermoon(dt=None):
     DATEfm, Dfm = next_full_moon(dt)
 
     # find distance of next perigee and apogee (for Espenak definition)
-    DATEp, Dp = next_perigee(dt)
+    DATEp, Dp = next_perigee(DATEfm-timedelta(days=14))
     DATEa, Da = next_apogee(DATEp)
+
     RelativeDistance_thisorbit = (Da - Dfm) / (Da - Dp)
 
     # find closest perigee and furthest apogee of the year for (Nolle definition)
@@ -68,37 +70,13 @@ def next_supermoon(dt=None):
                'fullmoon': {'date': DATEfm, 'localdate': DATEfm.astimezone(get_localzone())},
                'perigee': {'date': DATEp, 'localdate': DATEp.astimezone(get_localzone()), 'distance': Dp},
                'full perigee delta seconds': perigeedelta,
-               'full perigee delta hours': perigeedelta/60/60,
+               'full perigee delta hours': perigeedelta/3600,
               }
-
     if not any(results['definitions'].values()):
         results = next_supermoon(dt=dt+timedelta(days=29))
-
     return (results)
 
-def yearly_supermoons(year):
-    jan1 = dt.datetime(year=year,month=1,day=1,hour=0,minute=0,tzinfo=utc)
-    result = next_supermoon(dt=jan1)
-    msgstr = f"{result['fullmoon']['localdate'].strftime('%a %b %d %Y %I:%M %p %Z')} ({result['fullmoon']['date'].strftime('%H:%M %Z')})"
-    print (f"The next supermoon will occur on {msgstr}" )
-    print("The next 8 supermoons are")
-    # dt = result['fullmoon']['date']
-    for i in range(0,12):
-        result = next_supermoon(dt=result['fullmoon']['date']+timedelta(days=1))
-        if result['fullmoon']['localdate'].year > year:
-            continue
-        msgstr = f"{result['fullmoon']['localdate'].strftime('%a %m %d %Y %I:%M %p %Z')} ({result['fullmoon']['date'].strftime('%H:%M %Z')})"
-        thelist = []
-        for definition, meets in result['definitions'].items():
-            if meets:
-               thelist.append(definition)
-        if len(thelist) == len(result['definitions'].values()):
-            thelist = ['all known definitions']
-        elif len(thelist) > 1:
-            thelist.insert(-1, 'and')
-        print (f"  {msgstr} according to {', '.join(thelist)}")
-
-def main(dt,moons=1, year=None):
+def main(dt,moons=1, year=None, perigee=None):
     for i in range(1,moons+1):
         result = next_supermoon(dt=dt)
         if year is not None and result['fullmoon']['date'].year != year:
@@ -114,6 +92,9 @@ def main(dt,moons=1, year=None):
             thelist.insert(-1, 'and')
         print (f"  {msgstr} according to {', '.join(thelist)}")
         dt = result['fullmoon']['date']+timedelta(days=29)
+        # pprint(result)
+        if perigee:
+            print(f"   perigee: {result['perigee']['localdate'].strftime('%m/%d/%Y %H:%M %Z')} ({result['full perigee delta hours']:.2f} hours from full moon)")
 
 if __name__ == '__main__':
     helpmsg = '''
@@ -137,6 +118,7 @@ Supermoon definitions used:
                                      epilog=helpmsg)
     parser.add_argument('year', type=int, nargs='?', default=None, help='find supermoons for this year (optional)')
     parser.add_argument('--cnt', type=int, default=1, help='moons to show')
+    parser.add_argument('-P', '--perigee', action='store_const', default=False, const=True,  help='include perigee time')
     args = parser.parse_args()
 
     if args.year is None:
@@ -147,12 +129,12 @@ Supermoon definitions used:
             print (f"The next {args.cnt} supermoons will be:")
         else:
             print("The next supermoon will be:")
-        main(dt=None, moons=args.cnt)
+        main(dt=None, moons=args.cnt, perigee=args.perigee)
     else:
         if 1900 <= args.year <= 2050:
             print (f"Supermoons during {args.year}:")
             jan1 = datetime(year=args.year, month=1, day=1, hour=0, minute=0, tzinfo=utc)
-            main(dt=jan1, moons=13, year=args.year)
+            main(dt=jan1, moons=13, year=args.year, perigee=args.perigee)
         else:
             print (f"Please provide a year between 1900 and 2050, got {args.year} (per JPL DE421) ")
             sys.exit(1)
